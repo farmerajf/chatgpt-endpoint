cf4bd4719a7f91e9a3d6b4efd2cb066a635dca36
Adam Farmer <adam.farmer@safetyculture.io>
2023-03-29 17:51:48 +1100

    Added environment variables

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# chatgpt-endpoint
+# ChatGPT Proxy
 A private endpoint that allows me to easy integrate other projects with ChatGPT
+
+## Setup
+Create a default .env file with the following
+```
+OPENAI_API_KEY=
+INTERNAL_API_KEY=
+PORT=
+```
+
+## Usage
+```
+curl --data 'Hello How are you doing?' -H 'Content-Type: text/plain' localhost:3000/
+```
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express');
-const app = express();
 
-app.use(express.json());
+const app = express();
+app.use(express.text());
+
+// Check for OpenAI API key
+if (process.env.OPENAI_API_KEY === undefined) {
+  console.log("Please set the OPENAI_API_KEY environment variable.");
+  process.exit(1);
+}
+
+// Check for internal API key
+if (process.env.INTERNAL_API_KEY === undefined) {
+  console.log("Please set the INTERNAL_API_KEY environment variable.");
+  process.exit(1);
+}
+
+// Check for port
+process.env.PORT = Number(process.env.PORT);
+if (process.env.PORT === undefined || process.env.PORT <= 0 || process.env.PORT > 65536) {
+  console.log("Please set the PORT environment variable.");
+  process.exit(1);
+}
 
 async function getResponse(message) {
   const configuration = new Configuration({
@@ -23,11 +43,16 @@ async function getResponse(message) {
 }
 
 app.post('/', async (req, res) => {
-  const { message } = req.body;
-  const result = await getResponse(message);
-  res.send(result.data.choices[0].text);
+  console.log("Got: " + req.body)
+  try {
+    const result = await getResponse(req.body);
+    console.log("Response: " + result.data.choices[0].text)
+    res.send(result.data.choices[0].text);
+  } catch (error) {
+    console.log("Error: " + error)
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+// app.listen(process.env.PORT, () => {
+//   console.log("Server listening on port " + process.env.PORT);
+// });
