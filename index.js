c3a83672a2e9f3ce1b3e6a4611389add9a6d7fda
Adam Farmer <adam.farmer@safetyculture.io>
2023-04-10 16:01:55 +1000

    Bumping conversation history length

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var conversationFilePath = "./conversation.txt";
 // Default prompt
 var defaultPrompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n"
 // Conversation max lines
-var conversationMaxLines = 200;
+var conversationMaxLines = 1000;
 
 
 // Check is a file exists
